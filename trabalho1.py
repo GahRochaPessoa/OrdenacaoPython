56920156651f11474d2ddbd4400a21a25b1a232b
GahRochaPessoa <gabrielrocha828@gmail.com>
2020-08-26 14:11:41 -0300

    tentativa de implementação da lib sys para a quebra das comparações

--- trabalho1.py
+++ trabalho1.py
@@ -1,62 +1,72 @@
 import random
 import timeit
+import sys
+
+sys.setrecursionlimit(10000)
 
 
-def bubbleSort(lista):
-    for x in range(len(lista)-1, 0, -1):
+def bubbleSort(lista2):
+    for x in range(len(lista2)-1, 0, -1):
         for i in range(x):
-            if lista[i] > lista[i+1]:
-                aux = lista[i]
-                lista[i] = lista[i+1]
-                lista[i+1] = aux
-    return lista
+            if lista2[i] > lista2[i+1]:
+                aux = lista2[i]
+                lista2[i] = lista2[i+1]
+                lista2[i+1] = aux
+    return lista2
 
 
-def quickSort(lista, iniVet, fimVet):
+def quickSort(lista1, iniVet, fimVet):
     i = iniVet
     j = fimVet
-    pivo = lista[fimVet]
+    pivo = lista1[fimVet]
 
     while i <= j:
-        while lista[i] < pivo:
+        while lista1[i] < pivo:
             i += 1
-        while lista[j] > pivo:
+        while lista1[j] > pivo:
             j -= 1
         if i <= j:
-            aux = lista[i]
-            lista[i] = lista[j]
-            lista[j] = aux
+            aux = lista1[i]
+            lista1[i] = lista1[j]
+            lista1[j] = aux
             i += 1
             j -= 1
     if iniVet < j:
-        quickSort(lista, iniVet, j)
+        quickSort(lista1, iniVet, j)
     if i < fimVet:
-        quickSort(lista, i, fimVet)
-    return lista
+        quickSort(lista1, i, fimVet)
+    return lista1
 
 
 lista = []
 for i in range(10000):
     lista.append(random.randint(1, 100000))
+lista1 = []
+lista2 = []
 menu = int(input('Digite um numero para as operações no menu\n'
-                 '1-Ordenar com BubbleSort\n'
-                 '2-Ordenar com QuickSort\n'
+                 '1-Ordenar com QuickSort\n'
+                 '2-Ordenar com BubbleSort\n'
                  '3-Sair\n'))
 while menu != 3:
     if menu == 1:
+        lista1 = lista
+        fim = len(lista1) - 1
         inicio = timeit.default_timer()
-        bubbleSort(lista)
+        quickSort(lista1, 0, fim)
         final = timeit.default_timer()
-        print('\nA lista Ordenada é:'+str(lista)+'\n')
+        print('\nA lista desOrdenada é:'+str(lista)+'\n')
+        print('\nA lista Ordenada é:'+str(lista1)+'\n')
         print('Duração: %f' % (final-inicio))
+
     elif menu == 2:
-        fim = len(lista) - 1
+        lista2 = lista
         inicio = timeit.default_timer()
-        quickSort(lista, 0, fim)
+        bubbleSort(lista2)
         final = timeit.default_timer()
-        print('\nA lista Ordenada é:'+str(lista)+'\n')
+        print('\nA lista Ordenada é:'+str(lista2)+'\n')
         print('Duração: %f' % (final-inicio))
+
     menu = int(input('Digite um numero para as operações no menu\n'
-                     '1-Ordenar com BubbleSort\n'
-                     '2-Ordenar com QuickSort\n'
+                     '1-Ordenar com QuickSort\n'
+                     '2-Ordenar com BubbleSort\n'
                      '3-Sair\n'))
